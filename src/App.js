c9866409a07789c8ecba933f9659f08b8e85e1b6
Illya1123 <21520565@gm.uit.edu.vn>
2024-03-15 23:05:04 +0700

    add lab1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import './App.css';
 import Bai1 from "./Bai1.js";
+import Bai4 from './Bai4.js';
+import Bai5 from './Bai5.js';
+import { FormDataProvider as FormDataProvider4 } from './FormDataContext4';
+import { FormDataProvider as FormDataProvider5 } from './FormDataContext5';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<Layout />}>
-          <Route index element={<Bai1 />} />
+          <Route path="bai1" element={<Bai1 />} />
+          <Route path="bai4" element={<FormDataProvider4><Bai4 /></FormDataProvider4>} />
+          <Route path="bai5" element={<FormDataProvider5><Bai5 /></FormDataProvider5>} />
         </Route>
       </Routes>
     </BrowserRouter>
