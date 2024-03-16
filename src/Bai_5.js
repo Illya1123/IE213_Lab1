847b0077ffa506fde8948cc3e8c9e05891eac0ec
REI <anhbao1809@gmail.com>
2024-03-16 16:16:59 +0700

    21522207_[IE213]_Thực hành lab1_Bài 4 + Bài 5

---
+++ src/Bai_5.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { FormDataContext } from './FormDataContext5.js';
+import './Bai_5.css';
+import FormInput from './FormInput5';
+
+function Bai_5() {
+  const { submitted } = React.useContext(FormDataContext);
+
+  return (
+    <div className="Bai5">
+      <div style={{ padding: '20px' }}>
+        {submitted ? (
+          <SubmittedContent />
+        ) : (
+          <div>
+            <FormInput />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+const SubmittedContent = () => {
+    const {
+        name,
+        guest,
+        address,
+        age,
+        date,
+        location,
+        gender,
+        method,
+        request,
+        eventType,
+    } = React.useContext(FormDataContext);
+    const formattedRequests = request.split('\n').map((line, index) => (
+        <React.Fragment key={index}>
+          {line}
+          <br />
+        </React.Fragment>
+      ));
+      const currentDate = new Date().toLocaleString();
+      const formattedDate = new Date(date).toLocaleDateString('en-GB');
+      return (
+        <div className="SubmittedContent">
+          <h2>THÔNG TIN ĐẶT CHỖ</h2>
+          <div className="content">
+            <div className="KH">
+            <h4>Thông tin khách hàng</h4>
+            <p>Họ tên: {name} - Độ tuổi: {age} / Giới tính: {gender}</p>
+            <p>Địa chỉ: {address}</p>
+            </div>
+            <div className="DC">
+            <h4>Thông tin đặt chỗ</h4>
+            <p>Số khách tham gia bữa tiệc: {guest} - Ngày đặt tiệc: {formattedDate}</p>
+            <p>Loại tiệc: - {eventType} / Địa điểm: {location}</p>
+            </div>
+            <div className="YC">
+            <h4><i>Các yêu cầu kèm theo: </i></h4>
+            <pre style={{ paddingLeft: '5%' }}>{formattedRequests}</pre>
+            </div>
+            <p style={{textAlign: "center"}}><i>Quý khách biết đến nhà hàng của chúng tôi qua:  {method.join(", ")}</i></p>
+            <p>Chúng tôi nhận được thông tin đặt chỗ của quý khách vào lúc: <b>{currentDate}</b></p>
+          </div>
+        </div>
+      );
+    };
+export default Bai_5;
+    
---
+++ src/FormInput4.js
@@ -0,0 +1,117 @@
+import React, { useContext } from 'react';
+import { FormDataContext } from './FormDataContext4';
+
+const FormInput = () => {
+    const {
+        studentName,
+        setstudentName,
+        teacher,
+        setteacher,
+        className,
+        setclassName,
+        date,
+        setDate,
+        method,
+        setMethod,
+        unfinishTask,
+        setunfinishTask,
+        submitted,
+        handleSubmit,
+    } = useContext(FormDataContext);
+
+  return (
+    <form onSubmit={handleSubmit}>
+        <div className="TitleB4">
+        <text>THEO DÕI HỌC TẬP</text>
+      </div>
+      <div className="Content2_Bai_4">
+        <label>Họ tên học sinh:</label>
+        <input
+          value={studentName}
+          onChange={(e) => setstudentName(e.target.value)}
+          className="Input1_Bai_4"
+          type="text"
+          name="studentName"
+          required
+          disabled={submitted}
+        />
+      </div>
+      <div className='Content2_Bai_4'>
+        <label>Giáo viên phụ trách:</label>
+        <input
+          value={teacher}
+          onChange={(e) => setteacher(e.target.value)}
+          className="Input1_Bai_4"
+          type="text"
+          name="teacher"
+          required
+          disabled={submitted}
+        />
+      </div>
+      <div className='Content1_Bai_4'>
+        <label>Lớp:</label>
+        <input
+          value={className}
+          onChange={(e) => setclassName(e.target.value)}
+          className="Input2_Bai_4"
+          type="text"
+          name="className"
+          required
+          disabled={submitted}
+        />
+
+        <label>Ngày:</label>
+        <input
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          className="Input2_Bai_4"
+          type="date"
+          name="date"
+          required
+          disabled={submitted}
+        />
+      </div>
+      <label>Những việc phân công chưa làm:</label>
+      <div className='Content_Bai_4'>
+        <textarea
+          value={unfinishTask}
+          onChange={(e) => setunfinishTask(e.target.value)}
+          className="Input3_Bai_4"
+          name="unfinishTask"
+          style={{ height: '80px' }}
+          required
+          disabled={submitted}
+        />
+      </div>
+      <label>Chọn hình thức hoàn thành</label>
+      <div className='Content_Bai_4'>
+      <input
+            className="Checkbox_Bai_4"
+            type="checkbox"
+            name="Tại lớp"
+            disabled={submitted}
+            checked={method === 'Tại lớp'} 
+            onChange={() => setMethod('Tại lớp')}
+          />
+        <label>Những việc chưa làm sẽ được hoàn thành ngay tại lớp</label>
+      </div>
+      <div className='Content_Bai_4'>
+      <input
+            className="Checkbox_Bai_4"
+            type="checkbox"
+            name="Tại nhà"
+            disabled={submitted}
+            checked={method === 'Tại nhà'} 
+            onChange={() => setMethod('Tại nhà')}
+          />
+        <label>Sẽ hoàn thành những việc chưa làm tại nhà và nộp lại cho giáo viên vào ngày hôm sau</label>
+      </div>
+      <div className="GhiNhan">
+        <button className="Submit_Bai+4" type="submit" disabled={submitted}>
+          Ghi Nhận
+        </button>
+      </div>
+    </form>
+  );
+};
+ export default FormInput;
